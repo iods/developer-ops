6f190031f23aa7b30168747abb2559e93114c87f
Rye Miller <millerrye17@gmail.com>
2020-10-28 00:28:43 -0600

    Update to the shell template.

--- lib/start.sh
+++ lib/start.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
-# Project:     Cloud Tools
-# Description: Starter file for writing bash scripts.
-# Author:      Rye Miller
 #
-#   ____  ____  ____  ____
-#  ||i  |||o  |||d  |||s  |
-#  ||___|||___|||___|||___|
-#  |/___\|/___\|/_ _\|/___\
+# start.sh
 #
-# https://github.com/iods/cloud-tools
+# A boilerplate that offers a basic template for writing
+# scripts. Whether it is the documentation block or the
+# default functions. This can be copied over and used as
+# a starter for scripts.
+#
+# Usage:
+#
+#   cp start.sh <new_script_name>.sh
+#
+# Note: It is recommended to include this information as
+# it adds context to your work.
+#
+# IMPORTANT: Hopefully this shit doesn't break anything, but
+# if it does. Mention it here.
+#
+#    ____  ____  ____  ____  ____  ____
+#  ||D  |||R  |||K  |||S  |||T  |||R  ||
+#  ||___|||___|||___|||___|||___|||___||
+#  |/___\|/___\|/___\|/___\|/___\|/___\|
+#
+# Version: 0.1 [2020-10-27]
+# Copyright: Copyright (c) 2020, Rye Miller
+#
+# https://github.com/iods/devops
